410fc2e9b5961c335d497735731dd896dab0080b
NAlexH2 <alex@nah-red.net>
2024-04-11 16:45:36 -0700

    adding vars

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 from src.data_grabber import DataGrabber
+import os
+from datetime import datetime
 
+API_URL = "https://busdata.cs.pdx.edu/api/getBreadCrumbs?vehicle_id="
+DATA_FOLDER = os.path.join("raw_data_files")
+DATA_MONTH_DAY = datetime.now().strftime("%m-%d")
+DATA_PATH = os.path.join(DATA_FOLDER, DATA_MONTH_DAY)
 
 if __name__ == "__main__":
     data_collect = DataGrabber()
     data_collect.data_grabber()
+    
