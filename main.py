bfe3402d80a96fa746553f6b5b9eb7f6bbc06c2b
NAlexH2 <alex@nah-red.net>
2024-04-14 21:14:52 -0700

    adding command line args for upload.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 import shutil
+import sys
 
 from datetime import datetime
 from src.grabber import DataGrabber
@@ -7,8 +8,13 @@ from src.uploadgdrive import upload_to_gdrive
 
 
 if __name__ == "__main__":
+    if "-U" in sys.argv:
+        print("\nUpload arg found. Will send to google drive nharris@pdx.edu.")
+
     data_collect = DataGrabber()
     data_collect.data_grabber()
-    upload_to_gdrive()
+    if "-U" in sys.argv:
+        upload_to_gdrive()
+        print("\nUpload to google drive completed.")
 
     # shutil.rmtree(DATA_FOLDER)
