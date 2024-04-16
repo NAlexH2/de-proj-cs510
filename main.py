688142d0111db459c8ae5d967ec00634380f90c4
NAlexH2 <alex@nah-red.net>
2024-04-15 18:46:06 -0700

    progress on emailer

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import shutil
 import sys
 
 from datetime import datetime
+from src.dataemailer import data_emailer
 from src.grabber import DataGrabber
 from src.uploadgdrive import upload_to_gdrive
 
@@ -13,6 +14,10 @@ if __name__ == "__main__":
 
     data_collect = DataGrabber()
     data_collect.data_grabber()
+    OK_size = data_collect.OK_response.size
+    bad_size = data_collect.bad_response.size
+    # data_emailer(ok_size=OK_size, bad_size=bad_size)
+
     if "-U" in sys.argv:
         upload_to_gdrive()
         print("\nUpload to google drive completed.")
--- src/dataemailer.py
+++ src/dataemailer.py
@@ -1,2 +1,32 @@
+import os, sys, tarfile
+
+
+script_dir = os.path.dirname(os.path.abspath(sys.argv[0]))
+
+if "/src" in script_dir:
+    from vars import DATA_MONTH_DAY, DATA_FOLDER, FULL_DATA_PATH
+else:
+    from src.vars import DATA_MONTH_DAY, DATA_FOLDER, FULL_DATA_PATH
+
+
+def tar_data():
+    print("\n")
+    tar = tarfile.open(f"{FULL_DATA_PATH}.tar", "w")
+    files = os.listdir(f"{FULL_DATA_PATH}")
+    files.sort()
+    for file in files:
+        tar.add(FULL_DATA_PATH + "/" + file, arcname=DATA_MONTH_DAY + "/" + file)
+    tar.close()
+
+
 def data_emailer(ok_size: int, bad_size: int) -> None:
-    pass
+    email = "nharris@pdx.edu"
+    message = """
+    Subject: test!
+        
+    This was a test :) from yourself :)"""
+
+
+if __name__ == "__main__":
+    # tar_data()
+    data_emailer(0, 0)
