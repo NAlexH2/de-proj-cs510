7973bc7a24d3af5dfca245aa09f8a09f74265869
NAlexH2 <alex@nah-red.net>
2024-04-19 10:13:32 -0700

    more comments

--- src/publisher.py
+++ src/publisher.py
@@ -1,11 +1,13 @@
-import os
-import sys
-import json
-import base64
-from time import sleep
+import os, sys, json, base64
 from google.oauth2 import service_account
 from googleapiclient.discovery import build
-import sys
+
+script_dir = os.path.dirname(os.path.abspath(sys.argv[0]))
+
+if "/src" in script_dir:
+    from vars import DATA_MONTH_DAY, FULL_DATA_PATH
+else:
+    from src.vars import DATA_MONTH_DAY, FULL_DATA_PATH
 
 SCOPES = ["https://www.googleapis.com/auth/pubsub"]
 SERVICE_ACCOUNT_FILE = "./data_eng_key/data-eng-auth-data.json"
@@ -23,6 +25,12 @@ def publisher():
     encoded_message_content = base64.b64encode(
         message_content.encode()
     ).decode()
+
+    # TODO: before you continue make sure you encode each files json data in base64
+    # like this:
+    # base64.b64encode(str(message_data).encode()).decode()
+    # then attach that to the body of the json object being passed in to the
+    # example below.
     message_data = {"messages": [{"data": encoded_message_content}]}
 
     # Convert the message data to a JSON string
@@ -32,6 +40,14 @@ def publisher():
     # .projects()
     # .topics()
     # .publish(topic=TOPIC_ID, body=message)
+
+    files = os.listdir(FULL_DATA_PATH)
+    files.sort()
+
+    # for loop it through all files created in raw_data_files
+    num_files = len(files)
+    for i in range(num_files):
+        pass
     return
 
 
