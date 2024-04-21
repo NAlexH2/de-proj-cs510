fe73e9c1cc3310946b014a949585bf624ac1439f
NAlexH2 <alex@nah-red.net>
2024-04-20 22:20:04 -0700

    removing multiprocessing

--- src/publisher.py
+++ src/publisher.py
@@ -1,4 +1,3 @@
-import multiprocessing
 import os, sys, json, base64
 from google.oauth2 import service_account
 from googleapiclient.discovery import build
@@ -19,9 +18,10 @@ TOPIC_ID = "topics/VehicleData"
 FULL_TOPIC_ID = f"{PROJECT_ID}/{TOPIC_ID}"
 
 
-def multithread_publisher(data_file):
+def publisher(data_file):
     print(
-        f"{curr_time_micro()} - Publishing file {data_file} -- pid: {os.getpid()}"
+        f"{curr_time_micro()} - Publishing file {data_file} -- pid: {os.getpid()}",
+        end="\n",
     )
     creds = service_account.Credentials.from_service_account_file(
         SERVICE_ACCOUNT_FILE, scopes=SCOPES
@@ -42,37 +42,18 @@ def multithread_publisher(data_file):
             .publish(topic=FULL_TOPIC_ID, body=message)
             .execute()
         )
-        if len(response["messageIds"]) > 0 and "-V" in sys.argv:
-            print(
-                f"{data_file} - File record {i+1} of {working_file_length} "
-                + f"published w/ id {response['messageIds'][0]} "
-                + f"-- pid: {os.getpid()}"
-            )
     print(f"{curr_time_micro()} - {data_file} published -- pid: {os.getpid()}")
     working_file.close()
 
     return
 
 
-def work_multi(files_list, num_procs):
-    p = multiprocessing.Pool(num_procs)
-
-    # maps each file name over one of the processors available. This is not done
-    # in order, and the file is chosen by the multiprocessing library in some
-    # fashion.
-    p.imap_unordered(multithread_publisher, files_list)
-
-    # Just gotta clean up once all files have been worked on.
-    p.close()
-    p.join()
-
-
 def publish_data():
-    concurrences = 2
-
     files_list = os.listdir(FULL_DATA_PATH)
     files_list.sort()
-    work_multi(files_list, concurrences)
+    for i in range(len(files_list)):
+
+        publisher(files_list[i])
 
     print(f"All files published")
 
