8f4efcfe37ccb939fa49f300703f93aca2269126
NAlexH2 <alex@nah-red.net>
2024-04-21 01:01:19 -0700

    bash updates

--- main_pull.sh
+++ main_pull.sh
@@ -1,14 +1,14 @@
 text_date() {
-    echo "[$(date +"%m/%d/%Y-%H:%M:%S.%N" | cut -c -23)]"
+    echo "[$(date +"%m-%d-%Y-%H:%M:%S.%N" | cut -c -23)]"
 }
 
-echo "$(text_date) DATA COLLECTION START" >> MAINLOG
+echo "$(text_date) DATA COLLECTION START" >> "MAINLOG-$(date +"%Y-%m-%d").txt"
 cd /home/nharris/de-proj-cs510/
-echo -e "$(text_date) cd into dir complete -> $(pwd)" >> MAINLOG
-git pull
-echo "$(text_date) git pull complete" >> MAINLOG
-echo "$(text_date) Starting python script" >> MAINLOG
-python main.py -U >> MAINLOG
-echo "$(text_date) DATA COLLECTION COMPLETE" >> MAINLOG
+echo -e "$(text_date) cd into dir complete -> $(pwd)" >> "MAINLOG-$(date +"%Y-%m-%d").txt"
+git pull >> "SUBLOG-$(date +"%Y-%m-%d").txt"
+echo "$(text_date) git pull complete" >> "MAINLOG-$(date +"%Y-%m-%d").txt"
+echo "$(text_date) Starting python script" >> "MAINLOG-$(date +"%Y-%m-%d").txt"
+python main.py -U >> "MAINLOG-$(date +"%Y-%m-%d").txt"
+echo "$(text_date) DATA COLLECTION COMPLETE" >> "MAINLOG-$(date +"%Y-%m-%d").txt"
 
 
--- subscriber_pull.sh
+++ subscriber_pull.sh
@@ -1,12 +1,12 @@
 text_date() {
-    echo "[$(date +"%m/%d/%Y-%H:%M:%S.%N" | cut -c -23)]"
+    echo "[$(date +"%m-%d-%Y-%H:%M:%S.%N" | cut -c -23)]"
 }
 
-echo "$(text_date) SUBSCRIBER ACK STARTING" >> SUBLOG
+echo "$(text_date) SUBSCRIBER ACK STARTING" >> "SUBLOG-$(date +"%Y-%m-%d").txt"
 cd /home/nharris/de-proj-cs510/
-echo -e "$(text_date) cd into dir complete -> $(pwd)" >> SUBLOG
-git pull
-echo "$(text_date) git pull complete" >> SUBLOG
-echo "$(text_date) Starting subscriber listener." >> SUBLOG
-echo "$(text_date) THIS IS GOING TO RUN AS LONG AS THE VM IS UP." >> SUBLOG
-python subsciber.py >> SUBLOG
+echo -e "$(text_date) cd into dir complete -> $(pwd)" >> "SUBLOG-$(date +"%Y-%m-%d").txt"
+git pull >> "SUBLOG-$(date +"%Y-%m-%d").txt"
+echo "$(text_date) git pull complete" >> "SUBLOG-$(date +"%Y-%m-%d").txt"
+echo "$(text_date) Starting subscriber listener." >> "SUBLOG-$(date +"%Y-%m-%d").txt"
+echo "$(text_date) THIS IS GOING TO RUN AS LONG AS THE VM IS UP." >> "SUBLOG-$(date +"%Y-%m-%d").txt"
+python subsciber.py >> "SUBLOG-$(date +"%Y-%m-%d").txt"
