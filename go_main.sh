572d573a77609de72721913499fe57874111cb22
NAlexH2 <alex@nah-red.net>
2024-04-24 18:39:59 -0700

    updates to script

--- go_main.sh
+++ go_main.sh
@@ -2,13 +2,13 @@ text_date() {
     echo "[$(date +"%m-%d-%Y-%H:%M:%S.%N" | cut -c -23)]"
 }
 
-echo "$(text_date) DATA COLLECTION START" >> "MAINLOG-$(date +"%Y-%m-%d").txt"
+echo "$(text_date) DATA COLLECTION START" > "MAINLOG-$(date +"%Y-%m-%d").txt"
 
 git pull >> "MAINLOG-$(date +"%Y-%m-%d").txt"
 echo "$(text_date) git pull complete" >> "MAINLOG-$(date +"%Y-%m-%d").txt"
 
 echo "$(text_date) Starting python script" >> "MAINLOG-$(date +"%Y-%m-%d").txt"
-python main.py -U -P >> "MAINLOG-$(date +"%Y-%m-%d").txt" 2>&1
+python main.py -T -P >> "MAINLOG-$(date +"%Y-%m-%d").txt" 2>&1
 
 echo "$(text_date) DATA COLLECTION COMPLETE" >> "MAINLOG-$(date +"%Y-%m-%d").txt"
 
