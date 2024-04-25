4a98474ece7a261f4eed9ef2174a7ee4dcffa4b4
NAlexH2 <alex@nah-red.net>
2024-04-25 09:31:09 -0700

    moved tardata

--- src/dataemailer.py
+++ src/dataemailer.py
@@ -7,24 +7,9 @@ from googleapiclient.discovery import build
 script_dir = os.path.dirname(os.path.abspath(sys.argv[0]))
 
 if "/src" in script_dir:
-    from utils import DATA_MONTH_DAY, FULL_DATA_PATH, curr_time_micro
+    from utils import DATA_MONTH_DAY, curr_time_micro
 else:
-    from src.utils import DATA_MONTH_DAY, FULL_DATA_PATH, curr_time_micro
-
-
-def tar_data():
-    file_exists = os.path.exists(f"{FULL_DATA_PATH}.tar")
-    if file_exists:
-        os.remove(f"{FULL_DATA_PATH}.tar")
-    tar = tarfile.open(f"{FULL_DATA_PATH}.tar", "w")
-    files = os.listdir(f"{FULL_DATA_PATH}")
-    files.sort()
-    for file in files:
-        file_path = os.path.join(FULL_DATA_PATH, file)
-        tar.add(file_path, arcname=file_path)
-        print(f"{curr_time_micro()} {file} added to tar.")
-    tar.close()
-    print(f"{curr_time_micro()} tar for {DATA_MONTH_DAY} complete.")
+    from src.utils import DATA_MONTH_DAY, curr_time_micro
 
 
 def data_emailer(ok_size: int, bad_size: int) -> None:
@@ -56,5 +41,4 @@ def data_emailer(ok_size: int, bad_size: int) -> None:
 
 
 if __name__ == "__main__":
-    tar_data()
-    # data_emailer(0, 0)
+    data_emailer(0, 0)
