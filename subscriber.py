ec9f3e2f1e4a4e8c9695b1efb8f06f04365a546a
NAlexH2 <alex@nah-red.net>
2024-04-26 23:39:16 -0700

    supports proper logging

--- subscriber.py
+++ subscriber.py
@@ -1,11 +1,14 @@
+import logging
 import json, os, time, sys, random
 from google.oauth2 import service_account
 from google.cloud import pubsub_v1
 from concurrent.futures import TimeoutError
 from src.utils import (
+    DATA_MONTH_DAY,
     SUBSCRIBER_DATA_PATH_JSON,
     SUBSCRIBER_FOLDER,
     curr_time_micro,
+    sub_logger,
 )
 
 SERVICE_ACCOUNT_FILE = "./data_eng_key/data-eng-auth-data.json"
@@ -16,19 +19,27 @@ TIMEOUT = 120
 
 class PipelineSubscriber:
     def __init__(self) -> None:
-        self.pubsub_creds = service_account.Credentials.from_service_account_file(
-            SERVICE_ACCOUNT_FILE
+        self.pubsub_creds = (
+            service_account.Credentials.from_service_account_file(
+                SERVICE_ACCOUNT_FILE
+            )
         )
-        self.subscriber = pubsub_v1.SubscriberClient(credentials=self.pubsub_creds)
+        self.subscriber = pubsub_v1.SubscriberClient(
+            credentials=self.pubsub_creds
+        )
         self.sub_path = self.subscriber.subscription_path(PROJECT_ID, SUB_ID)
         self.data_to_write: list[str] = []
         self.current_listener_records = 0
 
     def write_records_to_file(self):
         json_data = []
-        print()
-        print(f"{curr_time_micro()} Total records: {self.current_listener_records}")
-        print(f"{curr_time_micro()} Writing all records to a single file.")
+        sub_logger(message="")
+        sub_logger(
+            message=f"{curr_time_micro()} Total records: {self.current_listener_records}"
+        )
+        sub_logger(
+            message=f"{curr_time_micro()} Writing all records to a single file."
+        )
 
         while len(self.data_to_write) > 0:
             data_prep = self.data_to_write.pop()
@@ -55,10 +66,10 @@ class PipelineSubscriber:
         self.data_to_write.append(decoded_data)
         self.current_listener_records = len(self.data_to_write)
         if self.current_listener_records % 1000 == 0:
-            print(
-                f"{curr_time_micro()} Approximate records received so "
+            sub_logger(
+                message=f"{curr_time_micro()} Approximate records received so "
                 + f"far: {self.current_listener_records}",
-                end="\r",
+                prend="\r",
             )
         message.ack()
         return
@@ -77,30 +88,40 @@ class PipelineSubscriber:
 
 
 if __name__ == "__main__":
+    os.makedirs("logs", exist_ok=True)
+    logging.basicConfig(
+        format="",
+        filename=f"logs/SUBLOG-{DATA_MONTH_DAY}.log",
+        encoding="utf-8",
+        filemode="w",
+        level=logging.INFO,
+    )
     sub_worker = PipelineSubscriber()
     start_time = curr_time_micro()
     total_records_overall = 0
-    print(f"{start_time} Subscriber starting.")
+    sub_logger(message=f"{start_time} Subscriber starting.")
     while True:
         sub_worker.subscriber_listener()
         if sub_worker.current_listener_records > 0:
             sub_worker.write_records_to_file()
-            print(
-                f"{curr_time_micro()} Subscriber started at {start_time}. "
+            sub_logger(
+                message=f"{curr_time_micro()} Subscriber started at {start_time}. "
                 + f" Subscriber complete."
             )
             total_records_overall += sub_worker.current_listener_records
             sub_worker.current_listener_records = 0
         else:
-            print(f"{curr_time_micro()} No data received in the past 5 minutes.")
+            sub_logger(
+                message=f"{curr_time_micro()} No data received in the past 5 minutes."
+            )
             total_records_overall += sub_worker.current_listener_records
             sub_worker.current_listener_records = 0
 
-        print(
-            f"{curr_time_micro()} Have saved and received {total_records_overall} records up to this point."
+        sub_logger(
+            message=f"{curr_time_micro()} Have received and saved {total_records_overall} records up to this point."
         )
-        print(
-            f"{curr_time_micro()} Subscriber re-starting to continue listening for messages."
+        sub_logger(
+            message=f"{curr_time_micro()} Subscriber re-starting to continue listening for messages."
         )
         sub_worker.subscriber = pubsub_v1.SubscriberClient(
             credentials=sub_worker.pubsub_creds
