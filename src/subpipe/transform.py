d5c43d40dcb8ece4043ab72e2dc0daa7b6153cac
NAlexH2 <alex@nah-red.net>
2024-05-05 22:16:54 -0700

    preparing assertions

---
+++ src/subpipe/transform.py
@@ -0,0 +1,30 @@
+import logging, os, sys, json
+import pandas as pd
+from pathlib import Path
+
+if __name__ == "__main__":
+    sys.path.insert(0, str(Path(__file__).parents[2].absolute()))
+
+from src.utils.utils import (
+    DATA_MONTH_DAY,
+    SUBSCRIBER_DATA_PATH_JSON,
+    SUBSCRIBER_FOLDER,
+    curr_time_micro,
+    sub_logger,
+)
+
+
+def transform_setup(df: pd.DataFrame):
+    new_df = df.sort_values(["VEHICLE_ID", "METERS"], ascending=True)
+    new_df = new_df.loc[(new_df["VEHICLE_ID"] == 2917)]
+    new_df2 = df.sort_values(["VEHICLE_ID", "METERS"], ascending=True).loc[
+        (df["VEHICLE_ID"] == 4043)
+    ]
+    print("test")
+
+
+if __name__ == "__main__":
+    df = pd.read_json(os.path.join(SUBSCRIBER_FOLDER, "04-11.json"))
+    # df2 = pd.read_json(os.path.join(SUBSCRIBER_FOLDER, "04-11.json"))
+    # df = pd.concat([df, df2])
+    transform_setup(df)
