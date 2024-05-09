1187031c082d7235a0867abac0d458a92b399112
NAlexH2 <alex@nah-red.net>
2024-05-08 20:58:07 -0700

    writing out thoughts

--- src/subpipe/to-db.py
+++ src/subpipe/to-db.py
@@ -1,5 +1,7 @@
+import logging, os, sys, json
+import pandas as pd
+
 from pathlib import Path
-import logging, os, sys
 
 
 if __name__ == "__main__":
@@ -8,20 +10,69 @@ if __name__ == "__main__":
 from src.subpipe.validate import ValidateBusData
 from src.utils.utils import (
     DATA_MONTH_DAY,
-    SUBSCRIBER_DATA_PATH_JSON,
     SUBSCRIBER_FOLDER,
     curr_time_micro,
     sub_logger,
 )
 
+"""Here's the plan...
+    When operating with subscriber pipeline:
+        1. This takes in the collection of strings from subscriber.data_to_write
+        2. Turn it into a dict w/ something like....
+            json_data = []
+            while len(self.data_to_write) > 0:
+                data_prep = self.data_to_write.pop()
+                json_data.append(json.loads(data_prep))
+        3. Take this whole thing and load it into a pd dataframe much like
+           how pd.read_json does... some how. Maybe make fake file object
+        4. split dataframe into 2 separate dataframes to load into the tables
+            individually.
+        5. do this by converting the dataframe down back into a csv, then into
+            a fake file object, then use psycopg2 `copy_from`
+    
+    When operating independently:
+        1. Most the same, but loading from the files in a for loop first
+        2. The rest should be the same ish? We'll see... Get it working with
+            subscriber first.
+"""
+
 
 class DataToSQLDB:
-    def __init__(self) -> None:
-        pass
+    def __init__(self, jData: dict) -> None:
+        self.json_data: dict = jData
 
     def to_db_start(self):
         pass
 
 
 if __name__ == "__main__":
-    pass
+    logging.basicConfig(
+        format="",
+        filename=f"logs/DBTRANSFERLOG-{DATA_MONTH_DAY}.log",
+        encoding="utf-8",
+        filemode="a",
+        level=logging.INFO,
+    )
+    sub_logger(f"{curr_time_micro()} Sending data to SQL database.")
+    files = []
+    if os.path.exists(SUBSCRIBER_FOLDER):
+        files = os.listdir(SUBSCRIBER_FOLDER)
+        files.sort()
+
+        while len(files) > 0:
+            file = files.pop()
+            sub_logger(
+                f"\n{curr_time_micro()} Next file to transform in memory: {file}"
+            )
+            curr_file = open(os.path.join(SUBSCRIBER_FOLDER, file))
+            json_from_file = json.load(curr_file)
+            db_worker = DataToSQLDB(json_from_file)
+
+    else:
+        sub_logger(
+            f"{curr_time_micro()} Folder {SUBSCRIBER_FOLDER} does not exist. "
+            + f"Unable to send any data as it does not exist."
+        )
+        sys.exit(0)
+    sub_logger(f"{curr_time_micro()} Data transfer to SQL database complete!")
+    sys.exit()
