0382cf9d4d81aa0094eeac1ed78c1d7da3d7ba6c
NAlexH2 <alex@nah-red.net>
2024-05-08 19:27:52 -0700

    staging

--- src/subpipe/to-db.py
+++ src/subpipe/to-db.py
@@ -1,9 +1,11 @@
 from pathlib import Path
-import logging, os, sys, json
+import logging, os, sys
+
 
 if __name__ == "__main__":
     sys.path.insert(0, str(Path(__file__).parents[2].absolute()))
 
+from src.subpipe.validate import ValidateBusData
 from src.utils.utils import (
     DATA_MONTH_DAY,
     SUBSCRIBER_DATA_PATH_JSON,
@@ -11,3 +13,12 @@ from src.utils.utils import (
     curr_time_micro,
     sub_logger,
 )
+
+
+class DataToSQLDB:
+    def __init__(self) -> None:
+        pass
+
+
+if __name__ == "__main__":
+    pass
