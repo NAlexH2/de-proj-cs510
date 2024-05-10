7ff83f99347d3a4d7796915cfc5862d30f764752
NAlexH2 <alex@nah-red.net>
2024-05-09 20:08:00 -0700

    git guardian still mad, removing entirely

--- in-class-assignments/06 Store/load_inserts.py
+++ in-class-assignments/06 Store/load_inserts.py
@@ -9,7 +9,6 @@ import csv
 
 DBname = "postgres"
 DBuser = "postgres"
-DBpwd = "NO PW IN FILE BECAUSE OF GIT GUARDIAN"
 TableName = "CensusData"
 Datafile = "filedoesnotexist"  # name of the data file to be loaded
 CreateDB = False  # indicates whether the DB table should be (re)-created
