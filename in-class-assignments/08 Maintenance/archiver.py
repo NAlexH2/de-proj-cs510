b046f15295040875590b21d32947142a0866e7af
NAlexH2 <alex@nah-red.net>
2024-05-20 10:57:50 -0700

    progress

--- in-class-assignments/08 Maintenance/archiver.py
+++ in-class-assignments/08 Maintenance/archiver.py
@@ -3,9 +3,38 @@ from google.oauth2 import service_account
 from google.cloud import pubsub_v1
 from concurrent.futures import TimeoutError
 
+SERVICE_ACCOUNT_FILE = "./data_eng_key/data-eng-auth-data.json"
+PROJECT_ID = "data-eng-419218"
+SUB_ID = "archivetest-sub"
+TIMEOUT = 120
+
+pubsub_creds = service_account.Credentials.from_service_account_file(
+    SERVICE_ACCOUNT_FILE
+)
+subscriber = pubsub_v1.SubscriberClient(credentials=pubsub_creds)
+sub_path = subscriber.subscription_path(PROJECT_ID, SUB_ID)
+data_to_write: list[str] = []
+current_listener_records = 0
+
+
+def callback(message: pubsub_v1.subscriber.message.Message) -> None:
+    rcvd_data: bytes = message.data
+    decoded_data = rcvd_data.decode()
+    # data_to_write.append(decoded_data)
+    # current_listener_records = len(data_to_write)
+    message.ack()
+    return
+
 
 def archive_go():
-    pass
+    streaming_future = subscriber.subscribe(sub_path, callback=callback)
+    with subscriber:
+        try:
+            streaming_future.result(timeout=TIMEOUT)
+        except TimeoutError:
+            streaming_future.cancel()
+            streaming_future.result()
+            return
 
 
 if __name__ == "__main__":
