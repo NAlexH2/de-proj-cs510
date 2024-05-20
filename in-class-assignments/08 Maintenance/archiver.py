73d988423245eda56a074186197c476514022462
NAlexH2 <alex@nah-red.net>
2024-05-20 12:39:41 -0700

    progress

--- in-class-assignments/08 Maintenance/archiver.py
+++ in-class-assignments/08 Maintenance/archiver.py
@@ -1,7 +1,11 @@
+import json
+import os
 import pandas as pd
 from google.oauth2 import service_account
-from google.cloud import pubsub_v1
+from google.cloud import pubsub_v1, storage
 from concurrent.futures import TimeoutError
+
+from utils.utils import SUBSCRIBER_DATA_PATH_JSON, SUBSCRIBER_FOLDER
 
 SERVICE_ACCOUNT_FILE = "./data_eng_key/data-eng-auth-data.json"
 PROJECT_ID = "data-eng-419218"
@@ -15,6 +19,30 @@ subscriber = pubsub_v1.SubscriberClient(credentials=pubsub_creds)
 sub_path = subscriber.subscription_path(PROJECT_ID, SUB_ID)
 data_to_write: list[str] = []
 current_listener_records = 0
+
+
+def write_records_to_file():
+    json_data: list[dict] = []
+
+    while len(data_to_write) > 0:
+        data_prep = data_to_write.pop()
+        json_data.append(json.loads(data_prep))
+
+    if not os.path.exists(SUBSCRIBER_FOLDER):
+        os.makedirs(SUBSCRIBER_FOLDER)
+    if not os.path.exists(SUBSCRIBER_DATA_PATH_JSON):
+        with open(SUBSCRIBER_DATA_PATH_JSON, "w") as outfile:
+            json.dump(json_data, outfile, indent=4)
+    else:
+        with open(SUBSCRIBER_DATA_PATH_JSON, "r") as outfile:
+            existing_data = json.load(outfile)
+
+        existing_data.extend(json_data)
+
+        with open(SUBSCRIBER_DATA_PATH_JSON, "w") as outfile:
+            json.dump(existing_data, outfile, indent=4)
+
+    return
 
 
 def callback(message: pubsub_v1.subscriber.message.Message) -> None:
@@ -41,6 +69,12 @@ def archive_go():
             return
 
 
+def upload_blob(bucket_name, source_file_name, destination_blob_name):
+    pass
+
+
 if __name__ == "__main__":
     archive_go()
+    write_records_to_file()
+    upload_blob("a", "a", "a")
     print()
