22155bc9b77b359eace6e93c2d971434101eada3
NAlexH2 <alex@nah-red.net>
2024-05-20 10:44:29 -0700

    staging

---
+++ in-class-assignments/08 Maintenance/archiver.py
@@ -0,0 +1,12 @@
+import pandas as pd
+from google.oauth2 import service_account
+from google.cloud import pubsub_v1
+from concurrent.futures import TimeoutError
+
+
+def archive_go():
+    pass
+
+
+if __name__ == "__main__":
+    archive_go()
