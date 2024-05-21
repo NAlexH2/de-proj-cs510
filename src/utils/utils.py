c3812c847cf01cd519e3ead482259b6da06fe8c7
NAlexH2 <alex@nah-red.net>
2024-05-21 12:25:53 -0700

    adding new api endpoint. changing old to more specific name.

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -3,7 +3,8 @@ import logging, os, sys
 from datetime import datetime
 
 # Utilities used throughout
-API_URL = "https://busdata.cs.pdx.edu/api/getBreadCrumbs?vehicle_id="
+BREADCRUMB_API_URL = "https://busdata.cs.pdx.edu/api/getBreadCrumbs?vehicle_id="
+STOPID_API_URL = "https://busdata.cs.pdx.edu/api/getStopEvents?vehicle_num="
 DATA_FOLDER = os.path.join("raw_data_files")
 DATA_MONTH_DAY = datetime.now().strftime("%m-%d")
 FULL_DATA_PATH = os.path.join(DATA_FOLDER, DATA_MONTH_DAY)
