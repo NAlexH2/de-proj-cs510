c642801d0d5c2a9944ba21bacccde6eecf5c97ab
NAlexH2 <alex@nah-red.net>
2024-05-21 12:36:29 -0700

    adding stopid and organizing vars better

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -5,9 +5,13 @@ from datetime import datetime
 # Utilities used throughout
 BREADCRUMB_API_URL = "https://busdata.cs.pdx.edu/api/getBreadCrumbs?vehicle_id="
 STOPID_API_URL = "https://busdata.cs.pdx.edu/api/getStopEvents?vehicle_num="
-RAW_DATA_FOLDER = os.path.join("raw_data_files")
 DATA_MONTH_DAY = datetime.now().strftime("%m-%d")
+RAW_DATA_FOLDER = os.path.join("raw_data_files")
 RAW_DATA_PATH = os.path.join(RAW_DATA_FOLDER, DATA_MONTH_DAY)
+
+STOPID_DATA_FOLDER = os.path.join("stop_id_data")
+STOPID_DATA_PATH = os.path.join(STOPID_DATA_FOLDER, DATA_MONTH_DAY)
+
 SUBSCRIBER_FOLDER = os.path.join("subscriber_data_files")
 SUBSCRIBER_DATA_PATH_JSON = os.path.join(
     SUBSCRIBER_FOLDER, f"{DATA_MONTH_DAY}.json"
@@ -18,7 +22,7 @@ def curr_time_micro() -> str:
     return f"[{datetime.now().strftime('%m-%d-%Y-%H:%M:%S.%f')[:-3]}]"
 
 
-def mdy_time() -> str:
+def mdy_string() -> str:
     return datetime.now().strftime("%m-%d-%Y")
 
 
