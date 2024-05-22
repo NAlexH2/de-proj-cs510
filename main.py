a0fb5705c631f69ed83ad078a0ae2f7e0c3e8c38
NAlexH2 <alex@nah-red.net>
2024-05-21 22:28:34 -0700

    proper error logging

--- main.py
+++ main.py
@@ -95,8 +95,8 @@ if __name__ == "__main__":
             filemode="a",
             level=logging.ERROR,
         )
-        log_and_print(message="EXCEPTION THROWN!")
-        log_and_print(message=f"Traceback:\n{traceback.format_exc()}")
-        log_and_print(message=f"Exception as e:\n{e}")
+        logging.error(message="EXCEPTION THROWN!")
+        logging.error(message=f"Traceback:\n{traceback.format_exc()}")
+        logging.error(message=f"Exception as e:\n{e}")
 
     sys.exit()
