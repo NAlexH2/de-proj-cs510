96cb92bc3b84de9ed9ce67c7e7ad09f9a629ee3b
NAlexH2 <alex@nah-red.net>
2024-05-22 17:21:01 -0700

    fixing bc_sub.py

--- bc_subscriber.py
+++ bc_subscriber.py
@@ -2,23 +2,23 @@ import logging
 import json, os
 import time
 import traceback
-from src.subpipe.store import DataToSQLDB
+from src.subpipe.store import BCDataToSQLDB
 
 from google.oauth2 import service_account
 from google.cloud import pubsub_v1
 from concurrent.futures import TimeoutError
 from src.utils.utils import (
     DATA_MONTH_DAY,
-    SID_SUBSCRIBER_DATA_PATH_JSON,
-    SID_SUBSCRIBER_FOLDER,
+    BC_SUBSCRIBER_DATA_PATH_JSON,
+    BC_SUBSCRIBER_FOLDER,
     curr_time_micro,
     log_and_print,
 )
 
 SERVICE_ACCOUNT_FILE = "./data_eng_key/data-eng-auth-data.json"
 PROJECT_ID = "data-eng-419218"
-SID_SUB_ID = "StopDataRcvr"
-TIMEOUT = 1800
+BC_SUB_ID = "BreadCrumbsRcvr"
+TIMEOUT = 300
 
 
 class PipelineSubscriber:
@@ -31,15 +31,13 @@ class PipelineSubscriber:
         self.subscriber = pubsub_v1.SubscriberClient(
             credentials=self.pubsub_creds
         )
-        self.sub_path = self.subscriber.subscription_path(
-            PROJECT_ID, SID_SUB_ID
-        )
+        self.sub_path = self.subscriber.subscription_path(PROJECT_ID, BC_SUB_ID)
         self.data_to_write: list[str] = []
         self.current_listener_records = 0
 
     def store_to_sql(self, jData: list[dict]) -> None:
         log_and_print(f"Sending data to SQL database.")
-        db_worker = DataToSQLDB(jData)
+        db_worker = BCDataToSQLDB(jData)
         db_worker.to_db_start()
         log_and_print(f"Data transfer to SQL database complete!")
 
@@ -54,18 +52,18 @@ class PipelineSubscriber:
 
         log_and_print(message=f"Writing all records to a single file.")
 
-        if not os.path.exists(SID_SUBSCRIBER_FOLDER):
-            os.makedirs(SID_SUBSCRIBER_FOLDER)
-        if not os.path.exists(SID_SUBSCRIBER_DATA_PATH_JSON):
-            with open(SID_SUBSCRIBER_DATA_PATH_JSON, "w") as outfile:
+        if not os.path.exists(BC_SUBSCRIBER_FOLDER):
+            os.makedirs(BC_SUBSCRIBER_FOLDER)
+        if not os.path.exists(BC_SUBSCRIBER_DATA_PATH_JSON):
+            with open(BC_SUBSCRIBER_DATA_PATH_JSON, "w") as outfile:
                 json.dump(json_data, outfile, indent=4)
         else:
-            with open(SID_SUBSCRIBER_DATA_PATH_JSON, "r") as outfile:
+            with open(BC_SUBSCRIBER_DATA_PATH_JSON, "r") as outfile:
                 existing_data = json.load(outfile)
 
             existing_data.extend(json_data)
 
-            with open(SID_SUBSCRIBER_DATA_PATH_JSON, "w") as outfile:
+            with open(BC_SUBSCRIBER_DATA_PATH_JSON, "w") as outfile:
                 json.dump(existing_data, outfile, indent=4)
 
         # Where the db storage magic happens!
@@ -120,7 +118,9 @@ if __name__ == "__main__":
         start_time = curr_time_micro()
         total_records_overall = 0
 
-        log_and_print(message=f"{start_time} Subscriber starting.\n")
+        log_and_print(
+            message=f"{start_time} Subscriber '{BC_SUB_ID}' starting.\n"
+        )
 
         while True:
             sub_worker.subscriber_listener()
@@ -161,6 +161,6 @@ if __name__ == "__main__":
             filemode="a",
             level=logging.FATAL,
         )
-        logging.error(f"EXCEPTION THROWN!")
-        logging.error(f"Traceback:\n{traceback.format_exc()}")
-        logging.error(f"Exception as e:\n{e}")
+        log_and_print(f"EXCEPTION THROWN!")
+        log_and_print(f"Traceback:\n{traceback.format_exc()}")
+        log_and_print(f"Exception as e:\n{e}")
